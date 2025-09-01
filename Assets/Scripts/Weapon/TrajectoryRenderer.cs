20ea3a65febff790751b378e6524fa3ae4c96dc7
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-09-01 11:28:28 +0900

    [Fix] 궤도 정확도 향상

--- Assets/Scripts/Weapon/TrajectoryRenderer.cs
+++ Assets/Scripts/Weapon/TrajectoryRenderer.cs
@@ -6,6 +6,7 @@ public class TrajectoryRenderer : MonoBehaviour
 {
     [SerializeField] private LineRenderer lineRenderer;
     [SerializeField] private LayerMask obstacleLayerMask;
+    [SerializeField] private float castRadius = 0.4f;
     [SerializeField] private float reflectionRenderDistance = 1f;
 
     private void Awake()
@@ -25,7 +26,7 @@ public class TrajectoryRenderer : MonoBehaviour
         Vector2 start = transform.position;
         Vector2 end = GetMousePos();
         Vector2 dir = (end - start).normalized;
-        RaycastHit2D hit = Physics2D.Raycast(start, dir, 100f, obstacleLayerMask);
+        RaycastHit2D hit = Physics2D.CircleCast(start, castRadius, dir, 100f, obstacleLayerMask);
         AddLinePosition(start);
 
         if (hit.collider != null)
