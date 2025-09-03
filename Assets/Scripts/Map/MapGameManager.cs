b345db35e8e731108b62f927cfb577570c09b60d
0time12 <onr55@naver.com>
2025-09-03 11:40:38 +0900

    [feat] 시작 엔딩 화면 추가

--- Assets/Scripts/Map/MapGameManager.cs
+++ Assets/Scripts/Map/MapGameManager.cs
@@ -12,13 +12,22 @@ public class MapGameManager : MonoBehaviour
         // 'Space' 키를 누르면 점수 10점 추가
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            AddScore(10);
+            AddScore(1);
         }
     }
 
     public void AddScore(int amount)
     {
         currentScore += amount;
+        if (scoreDisplayManager != null)
+        {
+            scoreDisplayManager.UpdateScore(currentScore);
+        }
+    }
+
+    public void reduceScore(int amount)
+    {
+        currentScore -= amount;
         if (scoreDisplayManager != null)
         {
             scoreDisplayManager.UpdateScore(currentScore);
