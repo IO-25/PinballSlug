80ef62be3cdf3d9f25152d5245832753645f43b9
NineTheDivine <1567wkddnjs@gmail.com>
2025-09-05 21:05:04 +0900

    [Fix] Option Bug Fixed

--- Assets/Scripts/UI/PauseButton.cs
+++ Assets/Scripts/UI/PauseButton.cs
@@ -5,18 +5,10 @@ using UnityEngine.UI;
 
 public class PauseButton : MonoBehaviour
 {
-    public Button pauseButton;
-    public Button resumeButton;
     public GameObject settingsPanel;
 
     void Start()
     {
-        if (PauseManager.Instance != null)
-        {
-            pauseButton.onClick.AddListener(PauseManager.Instance.PauseGame);
-            resumeButton.onClick.AddListener(PauseManager.Instance.ResumeGame);
-        }
-        
         PauseManager.Instance.SetSettingsPanel(settingsPanel);
     }
 }
